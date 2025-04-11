94d3a2e2f137758d42c99c5c9c319018fb110ff7
noobmaester <joshstilez@gmail.com>
2025-04-11 23:30:15 +0200

    feat(config): add experimental allowedDevOrigins for local development

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+      'http://localhost:3000',   // Default Next.js local development server
+      'http://172.17.112.1:3000', // Default Next.js Network development server
+      'local-origin.dev', 
+      '*.local-origin.dev',
+    ],
+  },
 };
 
 export default nextConfig;
