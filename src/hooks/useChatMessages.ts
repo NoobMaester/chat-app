70702ee9ed358d4f0c94748c4a87861dec49c122
noobmaester <joshstilez@gmail.com>
2025-04-12 00:34:39 +0200

    feat(chat): implement useChatMessages hook for real-time message retrieval

---
+++ src/hooks/useChatMessages.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import {db} from "../firebase/config";
+import { collection, onSnapshot, orderBy, query, Timestamp } from "firebase/firestore";
+
+export interface ChatMessage {
+    id: string;
+    text: string;
+    senderId: string;
+    timestamp: Timestamp;
+}
+
+export default function useChatMessages() {
+    const [messages, setMessages] = useState<ChatMessage[]>([]);
+
+    useEffect(() => {
+        const q = query(collection(db, 'messages'), orderBy('timestamp', 'asc'));
+
+        const unsubscribe = onSnapshot(q, (snapshot)=> {
+            const messagesData: ChatMessage[] = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...(doc.data() as Omit<ChatMessage, 'id'>)
+        }))
+            setMessages(messagesData);
+        })
+        return () => unsubscribe();
+    }, []);
+
+    return messages;
+}
